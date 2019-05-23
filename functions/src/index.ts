3621bdb55674223fa88aa7768264ad5c2c205fa5
Atiiga <ismailoc7@mail.com>
2019-05-23 16:04:10 +0000

    Cloud function updated

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,27 +1,47 @@
 import * as functions from 'firebase-functions';
 
 export const addLocation =  functions.database
-.ref('gps_devices/{DeviceID}').onUpdate((Change, context)=> {
-    const Pcoords = Change.before.val();
-    const Plat =Pcoords.latitude;
-    const Plng =Pcoords.longitude;
-    const Ncoords = Change.after.val();
-    const Nlat =Ncoords.latitude;
-    const Nlng =Ncoords.longitude;
-    console.log(Pcoords);
-    console.log(Ncoords);
-   /*var Track = {
-        Plat,
-        Plng,
-    };*/
-    if ((Plat-Nlat<=2||-2)||(Plng-Nlng<=2||-2)){
-        console.log(Plat-Nlng);
-        console.log(Plng-Nlng);
-        const timeUpdated = Date.now();
-        console.log(timeUpdated);
+.ref('gps_devices/{DeviceID}').onUpdate(async(Change, context)=> {
+    try {
+        const Pcoords = await Change.before.val();
+        const Plat =Pcoords.Latitute;
+        const Plng =Pcoords.Longitute;
+        const Ncoords = await Change.after.val();
+        const Nlat =Ncoords.Latitute;
+        const Nlng =Ncoords.Longitute;
+        console.log(Plat);
+        console.log(Plng);
+        console.log(Nlat);
+        console.log(Nlng);
+        console.log(Pcoords);
+        console.log(Pcoords);
+        const Latdif = Plat - Nlat;
+        const lngdif = Plng - Nlng;
 
-      return   Pcoords.ref.child('track').set({Plat,Plng,timeUpdated});
-    };
+        console.log(Latdif);
+        console.log(lngdif);
+        const timeUpdated = new Date().toLocaleString();
+        console.log(timeUpdated);
+    
+       // if ((Latdif<=2||-2)||(lngdif<=2||-2)){}
+
+        if (((Latdif>=2)||(lngdif>=2))||((Latdif<=-2)||(lngdif<=-2))){
+  
+          return   Change.after.ref.child(`track/${timeUpdated}`).set({
+          //return   Change.after.ref.child(`track/1223`).set({
+
+              Latitude:Plat, 
+              Longitude:Plng,
+              Time:timeUpdated});
+        }
+        else{
+            return null
+        }
+        
+    } catch (error) {
+        console.log(error)
+    }
+
 });
 
 

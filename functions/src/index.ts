aa2b894164fe4b53cac947e52945580cefc0b8ea
Atiiga <ismailoc7@mail.com>
2019-05-24 06:55:23 +0000

    Cloud function distance calculation added and update on the if statemen

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,17 @@
 import * as functions from 'firebase-functions';
 
+
+let prLat =0.000000
+let prLng =0.000000
+
+
+
 export const addLocation =  functions.database
 .ref('gps_devices/{DeviceID}').onUpdate(async(Change, context)=> {
     try {
+        console.log(prLat);
+        console.log(prLng);
+
         const Pcoords = await Change.before.val();
         const Plat =Pcoords.Latitute;
         const Plng =Pcoords.Longitute;
@@ -15,23 +24,41 @@ export const addLocation =  functions.database
         console.log(Nlng);
         console.log(Pcoords);
         console.log(Pcoords);
-        const Latdif = Plat - Nlat;
-        const lngdif = Plng - Nlng;
+        const Latdif = prLat - Nlat;
+        const Lngdif = prLng - Nlng;
 
         console.log(Latdif);
-        console.log(lngdif);
+        console.log(Lngdif);
         const timeUpdated = new Date().toLocaleString();
         console.log(timeUpdated);
     
-       // if ((Latdif<=2||-2)||(lngdif<=2||-2)){}
+        //Calculating for the distance between two coordinates 
+        const R = 6371e3; // Earth Radius in metres
+        const  RprLant = prLat*Math.PI / 180; //previous latitude in radians
+        const  RNlat  = Nlat*Math.PI/180;    //Current latitude in radians
+        const  RLatdif = Latdif*Math.PI/180;  // Latitude difference in radians
+        const  RLngdif = Lngdif*Math.PI/180;  // Longitude difference in radians
 
-        if (((Latdif>=2)||(lngdif>=2))||((Latdif<=-2)||(lngdif<=-2))){
-  
+        const a = Math.sin(RLatdif/2)*Math.sin(RLatdif/2) +
+                Math.cos(RprLant)*Math.cos(RNlat)*
+                Math.sin(RLngdif/2)*Math.sin(RLngdif/2);
+
+        const c = Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
+
+        const d = R*c; // d= distance in meters
+        console.log(d);
+
+        if (d >= 1000){
+            prLat = Nlat;
+            prLng = Nlng;
+            console.log(prLat);
+            console.log(prLng);
+
           return   Change.after.ref.child(`track/${timeUpdated}`).set({
           //return   Change.after.ref.child(`track/1223`).set({
 
-              Latitude:Plat, 
-              Longitude:Plng,
+              Latitude:Nlat, 
+              Longitude:Nlng,
               Time:timeUpdated});
         }
         else{
@@ -41,6 +68,7 @@ export const addLocation =  functions.database
     } catch (error) {
         console.log(error)
     }
+
 
 });
 

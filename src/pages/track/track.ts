85deb9acf9ad51d983e1c10543c8b36e98ee2fab
Atiiga <ismailoc7@mail.com>
2019-05-26 18:07:39 +0000

    Multiple Markers Added updated

--- src/pages/track/track.ts
+++ src/pages/track/track.ts
@@ -112,68 +112,16 @@ RecordedTrack(){
       });
       
     }
-
-   PlacesCoords(){
-          //this make the user load that app is loading the map
-    let loader = this.loadingCtrl.create({
-      content: "Please wait...",
-      duration: 15000,
-         });
-    loader.present();//start loader
-    this.afAuth.authState.subscribe((auth)=>{
-      this.currentUserId =auth.uid;
-      //get Device id
-      const deviceidRef: firebase.database.Reference = firebase.database().ref(`/devices/${this.currentUserId}`);
-      deviceidRef.on(`value`, devicesSnapShot =>{
-      this.deviceId = devicesSnapShot.val().device_id;
-
-      //get date
-      const date = new Date()
-      let day = date.getDate().toString();
-      let month = (date.getMonth()+1).toString();
-      let  year = date.getFullYear().toString();
-      let hours = date.getHours().toLocaleString();
-      let minutes = date.getMinutes().toLocaleString();
-      if (month.length < 2) month = '0' + month;
-      if (day.length < 2) day = '0' + day;
-      let dateFormat = year + '-'+ month +'-' + day
-        console.log(dateFormat);
-        //reference to places coords
-      const GpsDataRef: firebase.database.Reference = firebase.database().ref(`/gps_devices/${this.deviceId}/track/${dateFormat}`);
-        GpsDataRef.on(`value`,DataSnapshot=>{
-          this.Places = [];
-          DataSnapshot.forEach(snap=>{
-            this.Places.push({lat: parseFloat(snap.val().Latitude), lng: parseFloat(snap.val().Longitude)});
-          });
-          console.log(this.Places);
-        });
-      });
-    });
-  
-
-    const Route = new google.maps.Polyline({
-      path: this.Places,
-      geodesic: true,
-      strokeColor: '#FF0000',
-      strokeOpacity: 1.0,
-      strokeWeight: 9
-    });
-
-     Route.setMap(this.map);
-    loader.dismiss();//start loader
-
-    return this.Places;   
-  }
    
   dropRoute(){
 
-          //this make the user load that app is loading the map
-          let loader = this.loadingCtrl.create({
-            content: "Please wait...",
-            duration: 15000,
-               });
-          loader.present();//start loader
-    this.afAuth.authState.subscribe((auth)=>{
+     //this make the user load that app is loading the map
+     let loader = this.loadingCtrl.create({
+      content: "Please wait...",
+      duration: 15000,
+      });
+      loader.present();//start loader
+      this.afAuth.authState.subscribe((auth)=>{
       this.currentUserId =auth.uid;
       //get Device id
       const deviceidRef: firebase.database.Reference = firebase.database().ref(`/devices/${this.currentUserId}`);
